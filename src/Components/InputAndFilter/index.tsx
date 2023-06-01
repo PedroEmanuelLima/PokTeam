a593fd1131437ffb1396bd7eed342e434dac91e8
Pedro Emanuel <pa69731@gmail.com>
2023-05-31 21:08:35 -0300

    style: Tipo de pokemon selecionado no filtro e disposição do botão de filtro

--- src/Components/InputAndFilter/index.tsx
+++ src/Components/InputAndFilter/index.tsx
@@ -6,18 +6,18 @@ import {
     UncontrolledDropdown,
     DropdownToggle,
     DropdownMenu,
-    DropdownItem
+    DropdownItem,
 } from 'reactstrap';
-import { BsSearch, BsFilterCircleFill } from "react-icons/bs";
+import { BsSearch, BsFilterCircleFill, BsXCircle } from "react-icons/bs";
 
 import './styles.css';
 
-interface IInpuAndFilter{
+interface IInpuAndFilter {
     setTypeSelected: React.Dispatch<React.SetStateAction<string>>,
     typeSelected: string,
     typesPokemons: string[],
     searchValue: string,
-    setSearchValue:  React.Dispatch<React.SetStateAction<string>>,
+    setSearchValue: React.Dispatch<React.SetStateAction<string>>,
     handleSearchPokemon(): Promise<void>,
 }
 
@@ -33,13 +33,55 @@ export default function InpuAndFilter({
     return (
         <form className="containerInput">
             <InputGroup className="input-goup">
+                <UncontrolledDropdown
+                    className="me-2"
+                    direction="down"
+                >
+                    <DropdownToggle color="transparent" className="drop-toggle" caret>
+                        <span className="line-type">
+                            {typeSelected.trim() ? typeSelected : 'tipo'}
+                        </span>
+                    </DropdownToggle>
+                    <DropdownMenu className="menu-dropdown">
+                        {typeSelected.trim() &&
+                            <DropdownItem
+                                className="item-dropdown"
+                                onClick={() => setTypeSelected('')}
+                            >LIMPAR FILTRO</DropdownItem>
+                        }
+                        {typesPokemons.map((type) => (
+                            <div key={type}>
+                                <DropdownItem divider />
+                                <DropdownItem
+                                    style={{
+                                        backgroundColor: type === typeSelected ? '#2596be' : undefined,
+                                        color: type === typeSelected ? 'white' : undefined
+                                    }}
+                                    className="item-dropdown"
+                                    onClick={() => setTypeSelected(type)}
+                                >{type}</DropdownItem>
+                            </div>
+                        ))}
+                    </DropdownMenu>
+                </UncontrolledDropdown>
+
                 <Input
                     type="text"
                     placeholder="Nome ou especie"
                     onChange={(event) => setSearchValue(event.target.value)}
                     value={searchValue}
-                    className="text-primary"
+                    className="text-input"
                 />
+                {searchValue.trim() && <Button
+                    color="none"
+                    className="btn-clean"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setSearchValue('');
+                    }}
+                >
+                    <BsXCircle className="icon-clean" />
+                </Button>}
                 <Button
                     type="submit"
                     color="warning"
@@ -52,34 +94,6 @@ export default function InpuAndFilter({
                     <BsSearch className="icon-search" />
                 </Button>
             </InputGroup>
-
-            <UncontrolledDropdown
-                className="me-2"
-                direction="down"
-            >
-                <DropdownToggle color="transparent" className="drop-toggle">
-                    <BsFilterCircleFill className="icon-filter" />
-                </DropdownToggle>
-                <DropdownMenu className="menu-dropdown">
-                    <DropdownItem
-                        className="item-dropdown"
-                        onClick={() => setTypeSelected('')}
-                    >LIMPAR FILTRO</DropdownItem>
-                    {typesPokemons.map((type) => (
-                        <div key={type}>
-                            <DropdownItem divider />
-                            <DropdownItem
-                                style={{
-                                    backgroundColor: type === typeSelected ? '#2596be' : undefined,
-                                    color: type === typeSelected ? 'white' : undefined
-                                }}
-                                className="item-dropdown"
-                                onClick={() => setTypeSelected(type)}
-                            >{type}</DropdownItem>
-                        </div>
-                    ))}
-                </DropdownMenu>
-            </UncontrolledDropdown>
         </form>
     )
 }
